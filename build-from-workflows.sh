a1db0a28f838d52492dd8faaf2b9f44d86fb0db1
Peter Wilson <git-commit-by@peterwilson.cc>
2026-07-15 14:17:02 +1000

    Revert "Comment out pushes for testing."
    This reverts commit 05c1dadba94d3178b4ba0f8c2f1f58448bceb2d6.

--- build-from-workflows.sh
+++ build-from-workflows.sh
@@ -245,7 +245,7 @@ while read commit; do
 			# Delete the tag locally
 			git tag -d $commitTag;
 			# Delete the tag remotely
-			# git push origin :refs/tags/$commitTag;
+			git push origin :refs/tags/$commitTag;
 			# continue;
 		fi
 
@@ -255,5 +255,5 @@ while read commit; do
 done < $CURRENT_DIR/log-files/$BRANCH-workflow-commits-reversed.txt;
 
 cd $CURRENT_DIR/plugins/gutenberg-build;
-# git push origin $BRANCH:$BRANCH --force;
-# git push origin --tags;
+git push origin $BRANCH:$BRANCH --force;
+git push origin --tags;
