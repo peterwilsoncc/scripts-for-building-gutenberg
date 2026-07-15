1fd2638a1445e1a91be1e7cbb0a082ea3277f1d2
Peter Wilson <git-commit-by@peterwilson.cc>
2026-07-15 12:32:14 +1000

    More tries.

--- build-from-workflows.sh
+++ build-from-workflows.sh
@@ -53,10 +53,19 @@ if [[ $(git branch --list $BRANCH) ]]; then
 	git checkout $BRANCH;
 
 	# Get the latest commit's source hash from the commit body.
-	latestSourceCommit=$(git log -1 --pretty=%B | awk '/[Cc]ommit\/[0-9a-fA-F]{40}/ { match($0, /[0-9a-fA-F]{40}/); print substr($0, RSTART, RLENGTH); exit }');
+	# Get the full commit message
+	commitBody=$(git log -1 --pretty=%B);
+	# Trim all trailing whitespace (tabs, spaces, newlines) from the entire message
+	commitBodyTrimmed=$(echo "$commitBody" | sed -e :a -e '/^\s*$/d;/\n$/N;//ba');
+	# Get the last line
+	lastLine=$(echo "$commitBodyTrimmed" | tail -n 1);
+	# Extract the 40-character hash from the last line
+	latestSourceCommit=$(echo "$lastLine" | awk '{ match($0, /[0-9a-fA-F]{40}/); if (RSTART > 0) print substr($0, RSTART, RLENGTH) }');
 
 	if [ -z "$latestSourceCommit" ]; then
 		echo "Error: Could not extract source commit from last build commit" >&2
+		echo "Commit body was: $commitBody" >&2
+		echo "Last line was: $lastLine" >&2
 		exit 1
 	fi
 
