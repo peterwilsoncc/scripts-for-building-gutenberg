d158bfcf9c99450cb4e4898a76909b76416b9578
Peter Wilson <git-commit-by@peterwilson.cc>
2026-07-15 12:51:56 +1000

    Pull branch from origin.

--- build-branch-from-workflows.sh
+++ build-branch-from-workflows.sh
@@ -61,6 +61,9 @@ git checkout trunk;
 if [[ $(git branch --list $BRANCH) ]]; then
 	git checkout $BRANCH;
 
+	# Pull latest changes from origin
+	git pull origin $BRANCH || echo "Warning: Could not pull from origin";
+
 	# Get the latest commit's source hash from the commit body.
 	# Get the full commit message
 	commitBody=$(git log -1 --pretty=%B);
